f0554fb729fe6cdea767ceb1f900d8f04a24e62b
DJ0301 <dhananjayjoshi2003@gmail.com>
2023-04-01 13:40:15 +0530

    a

---
+++ collegegate/index.js
@@ -0,0 +1,16 @@
+//Server end which handles API requests
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3002;
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(port,() => {
+    console.log('listening at http://localhost:'+port);
+});
