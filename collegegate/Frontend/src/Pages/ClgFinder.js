f471eeb552cbd1751ed1e4c15bb53b1816d643b7
Scott-Reilly <scott.reilly30@gmail.com>
2023-04-02 14:33:34 +0530

    wip

--- collegegate/Frontend/src/Pages/ClgFinder.js
+++ collegegate/Frontend/src/Pages/ClgFinder.js
@@ -2,17 +2,33 @@ import React, { useState } from 'react';
 import './ChatWindow.css';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 function ClgFinder() {
+  const [currentItemIndex, ButtonPressed] = useState(0)
 
-  const [usrInput, usrHasInput ] = useState();
+  function ButtonPressed(){
 
-  const onChange = (event) =>{
-    const newUsrInput = event.target.value
-    usrHasInput(newUsrInput)
-    console.log(usrInput)
   }
 
-  return (
+
+  const items = ['F', 'I', 'L', 'M'];
+  const currentElement = document.getElementById('current-item');
+      currentElement.innerText = items[currentItemIndex];
+
+      const leftButton = document.getElementById('left-button');
+      leftButton.addEventListener('click', () => {
+        currentItemIndex = (currentItemIndex - 1 + items.length) % items.length;
+        currentElement.innerText = items[currentItemIndex];
+      });
+
+      const rightButton = document.getElementById('right-button');
+      rightButton.addEventListener('click', () => {
+        currentItemIndex = (currentItemIndex + 1) % items.length;
+        currentElement.innerText = items[currentItemIndex];
+   });
+
+
+  return (   
     <div className='Page'>
       <div className='LeftContainer'>
         {/* Content for the left side of the page */}
@@ -20,19 +36,23 @@ function ClgFinder() {
           <h2>Welcome to Collegegate</h2>
         </div>
       <section>
-        <h3>this is</h3>
+        <h3>This is</h3>
         <img src="Image.jpeg"></img>
       </section>
+
       <div className='navi'>
-          <button className='navi-btn'><ArrowBackIosIcon /></button>
+          <button className='navi-btn' onClick={}><ArrowBackIosIcon /></button>
+          
             <span class="navi-icon"><p>V</p></span>
+
           <button className='navi-btn'><ArrowForwardIosIcon /></button>
         </div>
+
 
       </div>
       <div className='RightContainer'>
         <div className="container">
-          <input type='text' className='usrInput' placeholder="Let's find you a college" onChange={onChange}></input>
+          <input type='text' className='usrInput' placeholder="Let's find you a college"></input>
         </div>
       </div>
     </div>
