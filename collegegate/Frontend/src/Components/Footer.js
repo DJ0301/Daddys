dfcff88a8faf1ffcbebd1a6d6a699ca559762f6a
Dylan Moraes <dylanmoraesdljdd@gmail.com>
2023-04-02 19:40:31 +0530

    css

--- collegegate/Frontend/src/Components/Footer.js
+++ collegegate/Frontend/src/Components/Footer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Link } from "react-router-dom";
 import './Footer.css';
+import logo from '../Assets/CollegeGate.png';
+
 
 function Footer() {
   return (
     <div>
     <div className='Footer'>
-      <GitHubIcon />
+    <img class= "logo-foot" src={logo} />
+    <div class="github-icon">      <GitHubIcon />
+      </div>
       <a href="https://www.freepik.com/free-vector/robot-character-constructor-set-illustration_40125205.htm">Image by Freepik</a>
     </div>
     <div>
