1cb575e6d2e54331c9bbf3692fd813a26e64a58c
Dylan Moraes <dylanmoraesdljdd@gmail.com>
2023-04-02 13:54:40 +0530

    left-side

--- collegegate/Frontend/src/Pages/ClgFinder.js
+++ collegegate/Frontend/src/Pages/ClgFinder.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import './ChatWindow.css';
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 function ClgFinder() {
 
-  const [usrInput, usrHasInput ] =useState();
+  const [usrInput, usrHasInput ] = useState();
 
   const onChange = (event) =>{
     const newUsrInput = event.target.value
@@ -13,9 +14,28 @@ function ClgFinder() {
 
   return (
     <div className='Page'>
-      <div class="container">
-        <input type='text' className='usrInput' placeholder="Let's find you a college" onChange={onChange}></input>
-    </div>
+      <div className='LeftContainer'>
+        {/* Content for the left side of the page */}
+        <div className='LeftContent'>
+          <h2>Welcome to Collegegate</h2>
+        </div>
+      <section>
+        <h3>this is</h3>
+        <img src="Image.jpeg"></img>
+      </section>
+      <div class="navi">
+        <span class="navi-icon"><ArrowBackIosIcon /></span>
+        <span class="navi-icon"><p>V</p></span>
+        <span class="navi-icon"><ArrowForwardIosIcon /></span>
+      </div>
+
+        
+      </div>
+      <div className='RightContainer'>
+        <div className="container">
+          <input type='text' className='usrInput' placeholder="Let's find you a college" onChange={onChange}></input>
+        </div>
+      </div>
     </div>
   );
 }
